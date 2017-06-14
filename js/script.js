64aeb3168d9e8ebfc23728aa08bcd87d7c95fae3
加藤　優奈 <yuvernacrew@gmail.com>
2017-06-15 01:53:43 +0900

    データの修正、チェックボックスの制御など

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 $(function(){
   var array =  [
     [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1 ,1, 1],
-    [0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0 ,0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1],
     [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1],
     [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1],
     [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
-    [1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1],
+    [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1],
+    [0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]
   ]
   // チェックボックス
@@ -20,15 +20,38 @@ $(function(){
     var valList = $checked.map(function(index, el){
       return $(this).val(); 
     });
+    console.log(valList);
 
+    //判定の処理
     if(valList.length == 2){
       if(array[valList[0]][valList[1]] == 0){
         $('.result-box').text('×');
+        $('label').addClass('btn-gray');
       }
-    }else if(valList.length == 1 || valList.length == 0){
-      $('.result-box').text('◯');
     }else{
-      $('.result-box').text('×');
+      $('.result-box').text('◯');
     }
+
+    //チェックボックスの制御
+    var $count = $("input[type=checkbox]:checked").length;
+    var $not = $('input[type=checkbox]').not(':checked');
+
+    if($count >= 2) {
+      $('label').addClass('btn-gray');
+      $not.attr("disabled",true);
+    }else{
+      $('label').removeClass('btn-gray');
+      $not.attr("disabled",false);
+    }
+  })
+
+  $('.reset-btn').click(function(){
+    $('.checkbox').prop('checked', false);
+    $('label').removeClass('btn-gray');
+    $('.result-box').text('○');
+
+    var $count = $("input[type=checkbox]:checked").length;
+    var $not = $('input[type=checkbox]').not(':checked')
+    $not.attr("disabled",false);
   })
 });
